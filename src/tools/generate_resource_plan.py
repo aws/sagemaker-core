ba07d32e6bdac0eacaef90c1c2cda7c7d5eaedaf
Mohan Gandhi <mohgan@amazon.com>
2024-03-20 19:21:07 +0000

    updated with custom wire

--- src/tools/generate_resource_plan.py
+++ src/tools/generate_resource_plan.py
@@ -9,6 +9,11 @@ This class is used to extract the resources and its actions from the service-2.j
 class ResourceExtractor:
     CLASS_METHODS = set(['create', 'add', 'start', 'register', 'import', 'list', 'get'])
     OBJECT_METHODS = set(['refresh', 'delete', 'update', 'stop', 'deregister'])
+
+    # Wire additional methods to resources
+    RESOURCE_TO_ADDITIONAL_METHODS = {
+        'Cluster': ['DescribeClusterNode', 'ListClusterNodes'],
+    }
     
     def __init__(self, file_path):
         with open(file_path, 'r') as file:
@@ -34,11 +39,12 @@ class ResourceExtractor:
             raise Exception(f"Protocol {self.protocol} not supported")
     
     def _filter_actions_for_resources(self, resources):
-        for resource in sorted(resources):
+        for resource in sorted(resources, key=len, reverse=True):
             # filter action in actions
             filtered_actions = set([a for a in self.actions if a.endswith(resource) or (a.startswith('List') and a.endswith(resource +'s'))])
             self.actions_under_resource.update(filtered_actions)
             self.resource_actions[resource] = filtered_actions
+
             self.actions = self.actions - filtered_actions
 
     def _extract_resources_and_its_api_actions(self):
@@ -77,27 +83,31 @@ class ResourceExtractor:
                                    'class_methods', 'object_methods', 
                                    'additional_methods', 'raw_actions'])
 
-        for resource, actions in self.resource_actions.items():
+        for resource, actions in sorted(self.resource_actions.items()):
             class_methods = set()
             object_methods = set()
             additional_methods = set()
-            for action in actions:
-                action = action.lower()
-                resource = resource.lower()
 
-                if action.split(resource)[0] == 'describe':
+            for action in actions:
+                action_low = action.lower()
+                resource_low = resource.lower()
+
+                if action_low.split(resource_low)[0] == 'describe':
                     class_methods.add('get')
                     object_methods.add('refresh')
                     continue
 
-                if action.split(resource)[0] in self.CLASS_METHODS:
-                    class_methods.add(action.split(resource)[0])
-                elif action.split(resource)[0] in self.OBJECT_METHODS:
-                    object_methods.add(action.split(resource)[0])
+                if action_low.split(resource_low)[0] in self.CLASS_METHODS:
+                    class_methods.add(action_low.split(resource_low)[0])
+                elif action_low.split(resource_low)[0] in self.OBJECT_METHODS:
+                    object_methods.add(action_low.split(resource_low)[0])
                 else:
                     additional_methods.add(action)
 
-            print(f"{resource} -- {sorted(class_methods)} -- {sorted(object_methods)} -- {sorted(additional_methods)} -- {sorted(actions)}")
+            # print(f"{resource} -- {sorted(class_methods)} -- {sorted(object_methods)} -- {sorted(additional_methods)} -- {sorted(actions)}")
+
+            if resource in self.RESOURCE_TO_ADDITIONAL_METHODS:
+                additional_methods.update(self.RESOURCE_TO_ADDITIONAL_METHODS[resource])
 
             new_row = pd.DataFrame({
                 'resource_name': [resource],
@@ -110,7 +120,7 @@ class ResourceExtractor:
 
             df = pd.concat([df, new_row], ignore_index=True)
 
-        df.to_csv('resource_actions.csv', index=False)
+        df.to_csv('resource_plan.csv', index=False)
 
 file_path = os.getcwd() + '/sample/sagemaker/2017-07-24/service-2.json'
 resource_extractor = ResourceExtractor(file_path)
