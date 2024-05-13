a4cf32e8e034c1035495b04212b4f87e966d2911
Pintao Zou <pintaoz@amazon.com>
2024-05-12 21:46:10 -0700

    Move code to generate_code function

--- src/tools/codegen.py
+++ src/tools/codegen.py
@@ -17,31 +17,17 @@ from src.tools.utils_codegen import UtilsCodeGen
 from src.tools.shapes_codegen import ShapesCodeGen
 from src.tools.resources_codegen import ResourcesCodeGen
 
-def generate_code(utils_code_gen: UtilsCodeGen, 
-                  shapes_code_gen: ShapesCodeGen,
-                  resources_code_gen: ResourcesCodeGen) -> None:
+def generate_code() -> None:
     """
     Generates the code for the given code generators.
 
     Note ordering is important, generate the utils and lower level classes first
     then generate the higher level classes.
 
-    Args:
-        utils_code_gen (UtilsCodeGen): The code generator for utility classes.
-        shapes_code_gen (ShapesCodeGen): The code generator for shape classes.
-        resources_code_gen (ResourcesCodeGen): The code generator for resource classes.
-
     Returns:
         None
     """
-    utils_code_gen.generate_utils()
-    shapes_code_gen.generate_shapes()
-    resources_code_gen.generate_resources()
-
-''' 
-Initializes all the code generator classes and triggers generator.
-'''
-if __name__ == "__main__":
+    
     # TODO: Inject service JSON file path & run through with all the sagemaker service JSON files
     with open(SERVICE_JSON_FILE_PATH, 'r') as file:
         service_json = json.load(file)
@@ -50,6 +36,12 @@ if __name__ == "__main__":
     shapes_code_gen = ShapesCodeGen(service_json=service_json)
     resources_code_gen = ResourcesCodeGen(service_json=service_json)
 
-    generate_code(utils_code_gen=utils_code_gen,
-                  shapes_code_gen=shapes_code_gen,
-                  resources_code_gen=resources_code_gen)
+    utils_code_gen.generate_utils()
+    shapes_code_gen.generate_shapes()
+    resources_code_gen.generate_resources()
+
+''' 
+Initializes all the code generator classes and triggers generator.
+'''
+if __name__ == "__main__":
+    generate_code()
