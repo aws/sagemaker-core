0b565bbaf4496420e69b12ee8d7b13569a81b0ae
Gokul A <166456257+nargokul@users.noreply.github.com>
2024-07-16 15:15:45 -0700

    Script Fixes

--- workflow_helper/compute_resource_coverage.py
+++ workflow_helper/compute_resource_coverage.py
@@ -2,11 +2,11 @@ import json
 
 
 def main():
-    json_file = 'data.json'
+    json_file = '../coverage.json'
 
     with open(json_file, 'r') as f:
         data = json.load(f)
-        print(data['files']['src/sagemaker_core/generated/resources.py']['percent_covered'])
+        print(data['files']['src/sagemaker_core/generated/resources.py']['summary']['percent_covered'])
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
