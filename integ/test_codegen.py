7f9562c328cb9ca48b159875a367acbd1c4bb19b
Gokul Anantha Narayanan <166456257+nargokul@users.noreply.github.com>
2024-07-16 14:36:43 -0700

    Code coverage (#109)
    * Resources Coverage
    
    * pip install coverage
    
    * Metric for Integration tests
    
    * Update step name
    
    * Update job names
    
    * Test Metric
    
    * Test metric
    
    * Put Metric Data step
    
    * Unit test metrics
    
    * Separate metric creation steps
    
    * Test Integration Metric
    
    * Shell Script for handling metrics
    
    * Test
    
    * Using success()
    
    * Test
    
    * Fix Message
    
    * Unit test metrics
    
    * Make unit tests on pull_request_target
    
    * Cleanup Sagemaker resources after integ test
    
    * AWS credentials for Unit test workflow
    
    * Test
    
    * JWT token permissions
    
    * Test
    
    * Test
    
    * Integration tests cleanup in finally block.
    
    * Metrics for coverage

--- integ/test_codegen.py
+++ integ/test_codegen.py
@@ -57,7 +57,6 @@ s3_input_path = "s3://{}/{}/data/{}".format(bucket, prefix, TRAIN_DATA)
 s3_output_path = "s3://{}/{}/output".format(bucket, prefix)
 image = image_uris.retrieve(framework="xgboost", region=region, version="latest")
 
-
 # To be replaced with representing strings when executing from personal account
 SUBNET_ONE = os.environ['SUBNET_ONE']
 SUBNET_TWO = os.environ['SUBNET_TWO']
@@ -67,143 +66,146 @@ SECURITY_GROUP_ONE = os.environ['SECURITY_GROUP_ONE']
 class TestSageMakerCore(unittest.TestCase):
 
     def test_training_and_inference(self):
-        os.environ["SAGEMAKER_ADMIN_CONFIG_OVERRIDE"] = ""
-        job_name_v3 = "xgboost-iris-" + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-        training_job = TrainingJob.create(
-            training_job_name=job_name_v3,
-            hyper_parameters={
-                "objective": "multi:softmax",
-                "num_class": "3",
-                "num_round": "10",
-                "eval_metric": "merror",
-            },
-            algorithm_specification=AlgorithmSpecification(
-                training_image=image, training_input_mode="File"
-            ),
-            role_arn=role,
-            input_data_config=[
-                Channel(
-                    channel_name="train",
-                    content_type="csv",
-                    compression_type="None",
-                    record_wrapper_type="None",
-                    data_source=DataSource(
-                        s3_data_source=S3DataSource(
-                            s3_data_type="S3Prefix",
-                            s3_uri=s3_input_path,
-                            s3_data_distribution_type="FullyReplicated",
-                        )
-                    ),
-                )
-            ],
-            profiler_config=ProfilerConfig(profiling_interval_in_milliseconds=1000),
-            output_data_config=OutputDataConfig(s3_output_path=s3_output_path),
-            resource_config=ResourceConfig(
-                instance_type="ml.m4.xlarge",
-                instance_count=1,
-                volume_size_in_gb=30,
-            ),
-            stopping_condition=StoppingCondition(max_runtime_in_seconds=600),
-        )
-        training_job.wait()
+        try:
+            os.environ["SAGEMAKER_ADMIN_CONFIG_OVERRIDE"] = ""
+            job_name_v3 = "xgboost-iris-" + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
+            training_job = TrainingJob.create(
+                training_job_name=job_name_v3,
+                hyper_parameters={
+                    "objective": "multi:softmax",
+                    "num_class": "3",
+                    "num_round": "10",
+                    "eval_metric": "merror",
+                },
+                algorithm_specification=AlgorithmSpecification(
+                    training_image=image, training_input_mode="File"
+                ),
+                role_arn=role,
+                input_data_config=[
+                    Channel(
+                        channel_name="train",
+                        content_type="csv",
+                        compression_type="None",
+                        record_wrapper_type="None",
+                        data_source=DataSource(
+                            s3_data_source=S3DataSource(
+                                s3_data_type="S3Prefix",
+                                s3_uri=s3_input_path,
+                                s3_data_distribution_type="FullyReplicated",
+                            )
+                        ),
+                    )
+                ],
+                profiler_config=ProfilerConfig(profiling_interval_in_milliseconds=1000),
+                output_data_config=OutputDataConfig(s3_output_path=s3_output_path),
+                resource_config=ResourceConfig(
+                    instance_type="ml.m4.xlarge",
+                    instance_count=1,
+                    volume_size_in_gb=30,
+                ),
+                stopping_condition=StoppingCondition(max_runtime_in_seconds=600),
+            )
+            training_job.wait()
 
-        fetched_training_job = TrainingJob.get(training_job_name=job_name_v3)
-        assert fetched_training_job.output_data_config.s3_output_path == s3_output_path
-        assert fetched_training_job.profiler_config.profiling_interval_in_milliseconds == 1000
+            fetched_training_job = TrainingJob.get(training_job_name=job_name_v3)
+            assert fetched_training_job.output_data_config.s3_output_path == s3_output_path
+            assert fetched_training_job.profiler_config.profiling_interval_in_milliseconds == 1000
 
-        creation_time_after = datetime.datetime.now() - datetime.timedelta(days=5)
+            creation_time_after = datetime.datetime.now() - datetime.timedelta(days=5)
 
-        resource_iterator = TrainingJob.get_all(creation_time_after=creation_time_after)
-        training_jobs = [job.training_job_name for job in resource_iterator]
+            resource_iterator = TrainingJob.get_all(creation_time_after=creation_time_after)
+            training_jobs = [job.training_job_name for job in resource_iterator]
 
-        assert len(training_jobs) > 1
-        assert job_name_v3 in training_jobs
+            assert len(training_jobs) > 1
+            assert job_name_v3 in training_jobs
 
-        model_data_url = fetched_training_job.model_artifacts.s3_model_artifacts
+            model_data_url = fetched_training_job.model_artifacts.s3_model_artifacts
 
-        key = f'xgboost-iris-test-{time.strftime("%H-%M-%S", time.gmtime())}'
-        print("Key:", key)
+            key = f'xgboost-iris-test-{time.strftime("%H-%M-%S", time.gmtime())}'
+            print("Key:", key)
 
-        model = Model.create(
-            model_name=key,
-            primary_container=ContainerDefinition(
-                image=image,
-                model_data_url=model_data_url,
-            ),
-            execution_role_arn=role,
-        )
+            model = Model.create(
+                model_name=key,
+                primary_container=ContainerDefinition(
+                    image=image,
+                    model_data_url=model_data_url,
+                ),
+                execution_role_arn=role,
+            )
 
-        # Testing Resource Chaining
-        endpoint_config = EndpointConfig.create(
-            endpoint_config_name=key,
-            production_variants=[
-                ProductionVariant(
-                    variant_name=key,
-                    initial_instance_count=1,
-                    instance_type="ml.m5.xlarge",
-                    model_name=model,  # Pass `Model`` object created above
-                )
-            ],
-        )
-        endpoint: Endpoint = Endpoint.create(
-            endpoint_name=key,
-            endpoint_config_name=endpoint_config,  # Pass `EndpointConfig` object created above
-        )
-        endpoint.wait_for_status("InService")
-        delete_all_sagemaker_resources()
+            # Testing Resource Chaining
+            endpoint_config = EndpointConfig.create(
+                endpoint_config_name=key,
+                production_variants=[
+                    ProductionVariant(
+                        variant_name=key,
+                        initial_instance_count=1,
+                        instance_type="ml.m5.xlarge",
+                        model_name=model,  # Pass `Model`` object created above
+                    )
+                ],
+            )
+            endpoint: Endpoint = Endpoint.create(
+                endpoint_name=key,
+                endpoint_config_name=endpoint_config,  # Pass `EndpointConfig` object created above
+            )
+            endpoint.wait_for_status("InService")
+        finally:
+            delete_all_sagemaker_resources()
 
     def test_intelligent_defaults(self):
-        os.environ["SAGEMAKER_ADMIN_CONFIG_OVERRIDE"] = (
-            self._setup_intelligent_default_configs_and_fetch_path()
-        )
-        job_name_v3 = "xgboost-test-intelligent-default-" + time.strftime(
-            "%Y-%m-%d-%H-%M-%S", time.gmtime()
-        )
+        try:
+            os.environ["SAGEMAKER_ADMIN_CONFIG_OVERRIDE"] = (
+                self._setup_intelligent_default_configs_and_fetch_path()
+            )
+            job_name_v3 = "xgboost-test-intelligent-default-" + time.strftime(
+                "%Y-%m-%d-%H-%M-%S", time.gmtime()
+            )
 
-        training_job = TrainingJob.create(
-            training_job_name=job_name_v3,
-            hyper_parameters={
-                "objective": "multi:softmax",
-                "num_class": "3",
-                "num_round": "10",
-                "eval_metric": "merror",
-            },
-            algorithm_specification=AlgorithmSpecification(
-                training_image=image, training_input_mode="File"
-            ),
-            role_arn=role,
-            input_data_config=[
-                Channel(
-                    channel_name="train",
-                    content_type="csv",
-                    compression_type="None",
-                    record_wrapper_type="None",
-                    data_source=DataSource(
-                        s3_data_source=S3DataSource(
-                            s3_data_type="S3Prefix",
-                            s3_uri=s3_input_path,
-                            s3_data_distribution_type="FullyReplicated",
-                        )
-                    ),
-                )
-            ],
-            output_data_config=OutputDataConfig(s3_output_path=s3_output_path),
-            resource_config=ResourceConfig(
-                instance_type="ml.m4.xlarge",
-                instance_count=1,
-                volume_size_in_gb=30,
-            ),
-            stopping_condition=StoppingCondition(max_runtime_in_seconds=600),
-        )
-        training_job.wait()
+            training_job = TrainingJob.create(
+                training_job_name=job_name_v3,
+                hyper_parameters={
+                    "objective": "multi:softmax",
+                    "num_class": "3",
+                    "num_round": "10",
+                    "eval_metric": "merror",
+                },
+                algorithm_specification=AlgorithmSpecification(
+                    training_image=image, training_input_mode="File"
+                ),
+                role_arn=role,
+                input_data_config=[
+                    Channel(
+                        channel_name="train",
+                        content_type="csv",
+                        compression_type="None",
+                        record_wrapper_type="None",
+                        data_source=DataSource(
+                            s3_data_source=S3DataSource(
+                                s3_data_type="S3Prefix",
+                                s3_uri=s3_input_path,
+                                s3_data_distribution_type="FullyReplicated",
+                            )
+                        ),
+                    )
+                ],
+                output_data_config=OutputDataConfig(s3_output_path=s3_output_path),
+                resource_config=ResourceConfig(
+                    instance_type="ml.m4.xlarge",
+                    instance_count=1,
+                    volume_size_in_gb=30,
+                ),
+                stopping_condition=StoppingCondition(max_runtime_in_seconds=600),
+            )
+            training_job.wait()
 
-        assert training_job.vpc_config.subnets == [
-            SUBNET_ONE,
-            SUBNET_TWO,
-        ]
-        assert training_job.vpc_config.security_group_ids == [SECURITY_GROUP_ONE]
-
-        delete_all_sagemaker_resources()
+            assert training_job.vpc_config.subnets == [
+                SUBNET_ONE,
+                SUBNET_TWO,
+            ]
+            assert training_job.vpc_config.security_group_ids == [SECURITY_GROUP_ONE]
+        finally:
+            delete_all_sagemaker_resources()
 
     def _setup_intelligent_default_configs_and_fetch_path(self) -> str:
         DEFAULTS_CONTENT = {
