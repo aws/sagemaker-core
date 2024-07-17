01361d9951a02a58edb3e1b0722e324fc1a843a5
Gokul A <166456257+nargokul@users.noreply.github.com>
2024-07-17 13:02:57 -0700

    Address review comments

--- workflow_helper/compute_resource_coverage.py
+++ workflow_helper/compute_resource_coverage.py
@@ -2,6 +2,10 @@ import json
 
 
 def main():
+    """
+    Every run of the pytest-cov command creates the report of coverages per class into a json file - i.e coverage.json
+    This file is being parsed to print the coverage for resources.py which will be used as a tracking metric
+    """
     json_file = 'coverage.json'
 
     with open(json_file, 'r') as f:
