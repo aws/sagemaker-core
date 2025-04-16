b006763dece7519f5c94581cf42d1e622263e914
Abdul Fatir <Abdulfatirs@gmail.com>
2025-04-16 23:00:30 +0200

    Fix logging behavior (#287)
    * Fix log level change
    
    * Remove handler before adding
    
    * code style
    
    * Update utils.py

--- src/sagemaker_core/main/utils.py
+++ src/sagemaker_core/main/utils.py
@@ -182,8 +182,11 @@ def get_textual_rich_logger(name: str, log_level: str = "INFO") -> logging.Logge
     """
     enable_textual_rich_console_and_traceback()
     handler = get_rich_handler()
-    logging.basicConfig(level=getattr(logging, log_level), handlers=[handler])
     logger = logging.getLogger(name)
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    logger.addHandler(handler)
+    logger.setLevel(getattr(logging, log_level))
 
     return logger
 
