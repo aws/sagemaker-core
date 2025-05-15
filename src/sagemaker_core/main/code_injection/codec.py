d1f3f64a9070070179a693e1d7d8df348ef8b54f
Roja Reddy Sareddy <rsareddy@amazon.com>
2025-05-15 15:35:53 -0700

    correct black formatting

--- src/sagemaker_core/main/code_injection/codec.py
+++ src/sagemaker_core/main/code_injection/codec.py
@@ -24,6 +24,7 @@ from sagemaker_core.main.code_injection.constants import (
 )
 from io import BytesIO
 
+
 def pascal_to_snake(pascal_str):
     """
     Converts a PascalCase string to snake_case.
@@ -244,11 +245,11 @@ def transform(data, shape, object_instance=None) -> dict:
         elif _member_type == MAP_TYPE:
             _map_type_shape = SHAPE_DAG[_member_shape]
             evaluated_value = _evaluate_map_type(data[_member_name], _map_type_shape)
-        elif _member_type == 'blob':
+        elif _member_type == "blob":
             blob_data = data[_member_name]
             if isinstance(blob_data, bytes):
                 evaluated_value = BytesIO(blob_data)
-            elif hasattr(blob_data, 'read'):
+            elif hasattr(blob_data, "read"):
                 # If it's already a file-like object, use it as is
                 evaluated_value = blob_data
             else:
